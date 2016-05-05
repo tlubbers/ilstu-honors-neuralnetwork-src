34e62edbc3bcc36bcff343ad77253f2e0a087f7f
alectroemel <alectroemel@hotmail.com>
2016-05-05 00:22:19 -0500

    added logic and csv that is all counties that have not voted yet

--- merge.py
+++ merge.py
@@ -8,29 +8,43 @@ def main():
 
 	final_rep = open ("final_rep.csv", "wt")
 	final_dem = open ("final_dem.csv", "wt")
+	final_unvoted = open ("final_unvoted.csv", "wt")
+
 
 	final_reader = list(csv.reader(final, delimiter=','))
 	rep_reader = list(csv.reader(rep, delimiter=','))
 	dem_reader = list(csv.reader(dem, delimiter=','))
 
 	for i in final_reader:	
+		rep_switcher = False
+		dem_switcher = False
+
 		for j in rep_reader: 
 			# this county has voted 
 			if i[0] == j[0]:
+				rep_switcher = True
 				mylist = i + j[1:]
 				for s in mylist:
 					final_rep.write(s+",")
 				final_rep.write("\n")
-
 		for k in dem_reader:
 			# this county has voted 
 			if i[0] == k[0]:
+				dem_switcher = False
 				mylist = i + k[1:]
 				for s in mylist:
 					final_dem.write(s+",")
 				final_dem.write("\n")
+	
+		# not in either, not voted for yet 
+		if rep_switcher == False and dem_switcher == False:
+			for s in i:
+				final_unvoted.write(s+',')
+			final_unvoted.write("\n")
+
 	final_dem.close()
 	final_rep.close()
+	final_unvoted.close()
 
 if __name__ == "__main__":
 	main()
