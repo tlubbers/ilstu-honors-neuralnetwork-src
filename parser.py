93d3ff2f8b818ef88248ba054c6e6077ff7fc650
Jmbrace <jmbrace@ilstu.edu>
2016-05-04 20:00:38 -0500

    Pushing data files

---
+++ parser.py
@@ -0,0 +1,91 @@
+
+def main():
+	pass
+# AGE040209D # Resident population total (July 1 - estimate) 2009
+# AGE050210D # Resident population: Median age (April 1 - complete count) 2010
+# AGE270210D # Resident population under 18 years (April 1 - complete count) 2010
+# AGE305210D # Resident population 18 to 44 years (April 1 - complete count) 2010
+# AGE770209D # Resident population 65 years and over (July 1 - estimate) 2009
+
+# AGN110207D # Average value of land and buildings per acre (NAICS) 2007 (adjusted)
+
+# ANC100209D # Persons by first ancestry reported - total 2005-2009
+
+# BNK010210D # Commercial banks and savings institutions (FDIC-insured) - number of offices (June 30) 2010
+# BNK050210D # Commercial banks and savings institutions (FDIC-insured) - total deposits (June 30) 2010
+
+# BPS030210D # New private housing units authorized by building permits - total 2010 (20,000-place universe)
+# BPS130210D # Valuation of new private housing units authorized by building permits 2010 (20,000-place universe)
+
+# BZN005209D # Private nonfarm establishments - forestry, fishing, hunting, and agriculture support (2007  NAICS 11) 2009
+# BZN075209D # Private nonfarm establishments - utilities (2007 NAICS 22) 2009
+# BZN110209D # Private nonfarm establishments - construction (2007 NAICS 23) 2009
+# BZN210209D # Private nonfarm establishments - manufacturing (2007 NAICS 31) 2009
+# BZN410209D # Private nonfarm establishments - retail trade (2007 NAICS 44) 2009
+# BZN450209D # Private nonfarm establishments - transportation and warehousing (2007 NAICS 48 & 49) 2009
+# BZN480209D # Private nonfarm establishments - information (2007 NAICS 51) 2009
+# BZN510209D # Private nonfarm establishments - finance and insurance (2007 NAICS 52) 2009
+# BZN550209D # Private nonfarm establishments - real estate, rental and leasing (2007 NAICS 53) 2009
+# BZN610209D # Private nonfarm establishments - professional, scientific, and technical services (2007 NAICS 54) 2009
+# BZN685209D # Private nonfarm establishments - educational services (2007 NAICS 61) 2009
+# BZN710209D # Private nonfarm establishments - health care and social assistance (2007 NAICS 62) 2009
+# BZN780209D # Private nonfarm annual payroll - arts, entertainment and recreation (2007 NAICS 71) 2009
+
+# CLF010210D # Civilian labor force 2010
+# CLF040210D # Civilian labor force unemployment rate 2010
+
+# CRM110208D # Number of violent crimes known to police 2008
+# CRM140208D # Number of murders and nonnegligent manslaughters known to police 2008
+# CRM240208D # Number of burglaries known to police 2008
+# CRM250208D # Number of larceny-thefts known to police 2008
+# CRM260208D # Number of motor vehicle thefts known to police  2008
+
+# EAN012207D # Earnings in all industries (NAICS, no code), average earnings per job 2007
+
+# EDU015208D # Public school enrollment Fall 2008-2009
+# EDU442209D # School enrollment - persons 3 years and over, male, enrolled in graduate or professional school: 2005-2009
+# EDU490209D # School enrollment - persons 3 years and over, female, enrolled in graduate or professional school: 2005-2009
+# EDU448209D # School enrollment - persons 3 years and over, male, not enrolled in school: 2005-2009
+# EDU496209D # School enrollment - persons 3 years and over, female, not enrolled in school: 2005-2009
+
+# HEA775207D # All persons under 65 years without health insurance, percent 2007
+
+# HIS020210D # Hispanic or Latino persons 2010 (complete count)
+
+# HSD110210D # Family households 2010 (complete count)
+
+# INC150209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) less than $10,000 in 2005-2009
+# INC170209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $10,000 to $14,999 in 2005-2009
+# INC180209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $15,000 to $19,999 in 2005-2009
+# INC190209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $20,000 to $24,999 in 2005-2009
+# INC200209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $25,000 to $29,999 in 2005-2009
+# INC210209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $30,000 to $34,999 in 2005-2009
+# INC220209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $35,000 to $39,999 in 2005-2009
+# INC240209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $40,000 to $44,999 in 2005-2009
+# INC250209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $45,000 to $49,999 in 2005-2009
+# INC270209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $50,000 to $59,999 in 2005-2009
+# INC280209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $60,000 to $74,999 in 2005-2009
+# INC300209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $75,000 to $99,999 in 2005-2009
+# INC310209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $100,000 to $124,999 in 2005-2009
+# INC320209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $125,000 to $149,999 in 2005-2009
+# INC340209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $150,000 to $199,999 in 2005-2009
+# INC350209D # Households with income in the past 12 months (in 2009 inflation-adjusted dollars) of $200,000 or more in 2005-2009
+# INC425209D # Mean household Social Security income in the past 12 months (in 2009 inflation-adjusted dollars) in 2005-2009
+# INC454209D # Households with Food Stamp/SNAP benefits in the past 12 months, total 2005-2009
+
+# IPE010209D # Median household income 2009
+# IPE120209D # People of all ages in poverty - percent 2009
+
+# POP010210D # Resident population (April 1 - complete count) 2010
+# POP040210D # Net change 2000 - 2010
+# POP060210D # Population per square mile 2010
+# POP150210D # Male population 2010 (complete count)
+# POP160210D # Female population 2010 (complete count)
+# POP230210D # Population of one race - White alone, not Hispanic or Latino 2010 (complete count)
+# POP250210D # Population of one race - Black or African American alone 2010 (complete count)
+# POP280210D # Population of one race - American Indian and Alaska Native alone 2010 (complete count)
+# POP320210D # Population of one race - Asian alone 2010 (complete count)
+
+
+if __name__ == "__main__":
+	main()
