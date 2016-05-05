f1fb9457dfa96870e19579ac8c7ef444d9763223
alectroemel <alectroemel@hotmail.com>
2016-05-05 13:03:45 -0500

    creating own validation function

--- honors_net.py
+++ honors_net.py
@@ -60,7 +60,7 @@ class Network(object):
 
 			# reader.pop()
 			for row in reader:
-				print (row[(self.input_size+2):(len(row)-2)])
+				print (row[(self.input_size+1):(len(row)-1)])
 				# print(row[1:(self.input_size+1)])
 				# format data
 				input_data = tuple(map(float, row[1:(self.input_size+1)]))
