114cf97df821b3aa5fdcbd3c69c1a96b659713f9
alectroemel <alectroemel@hotmail.com>
2016-05-06 07:28:51 -0500

    trying stuff

---
+++ validate3.py
@@ -0,0 +1,47 @@
+import sys
+from honors_net import Network 
+
+def main():
+
+	print ("this is the test")
+
+	if len(sys.argv) != 3:
+		print ("format: <filename> <# of outputs>")
+		exit() 
+
+	filedir = sys.argv[1]
+	os = int(sys.argv[2])
+	output_file = open (filedir[:-4] + "_3validations.csv", "wt")
+	
+	funct_types = ['sigmoid','tanh']
+
+	print ("lets start")
+	sys.stdout.flush()
+	
+	x = 70
+	while x <= 80:
+		print("yo")
+
+		for t in funct_types: 
+			for h in range(1,4): 
+				network = Network(input_size=69, output_size=os,number_of_layers=h+2, type_of_hidden_layer=t, net_bias=True, epochs=x)
+				network.prepare_trainer(filedir)
+				output_file.write(t + ",")
+				output_file.write(str(h))
+				output_file.write(",")
+				output_file.write(str(x))
+				output_file.write(",")
+				print ("cross validate")
+				output_file.write(str(network.cross_vaildate()))
+				output_file.write("\n")
+				print ("tick")
+				sys.stdout.flush()
+				output_file.flush()
+			print ("tock")
+		print ("tuck")
+		x += 10
+
+	output_file.close()
+	
+if __name__ == "__main__":
+	main()
